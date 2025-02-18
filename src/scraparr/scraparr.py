aefc0c4123f1ac47ce75723b88ad4158e32ebc22
Marc <mbg14.gaming@gmail.com>
2025-02-18 11:59:17 +0100

    Add Basic Auth and Bearer Authentication to Metrics Endpoint

--- src/scraparr/scraparr.py
+++ src/scraparr/scraparr.py
@@ -23,34 +23,38 @@ import scraparr.connectors
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-config = configparser.ConfigParser()
-config.read('scraparr/config.cnf')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('scraparr/config.cnf')
 
-path = config.get('GENERAL', 'path', fallback="/metrics")
-address = config.get('GENERAL', 'address', fallback="0.0.0.0")
-port = config.get('GENERAL', 'port', fallback=7100)
+PATH = CONFIG.get('GENERAL', 'path', fallback="/metrics")
+ADDRESS = CONFIG.get('GENERAL', 'address', fallback="0.0.0.0")
+PORT = CONFIG.get('GENERAL', 'port', fallback=7100)
+
+USERNAME = CONFIG.get('AUTH', 'username', fallback=None)
+PASSWORD = CONFIG.get('AUTH', 'password', fallback=None)
+BEARER_TOKEN = CONFIG.get('AUTH', 'token', fallback=None)
 
 metrics_app = make_wsgi_app()
-app = Middleware(metrics_app)
+app = Middleware(metrics_app, USERNAME, PASSWORD, BEARER_TOKEN)
 
 ACTIVE_CONNECTORS = ['SONARR', 'RADARR', 'PROWLARR']
 BEAUTIFUL_CONNECTORS = ", ".join(ACTIVE_CONNECTORS[:-1]) + " or " + ACTIVE_CONNECTORS[-1]
 
 if __name__ == '__main__':
-    if not any(section in config.sections() for section in ACTIVE_CONNECTORS):
+    if not any(section in CONFIG.sections() for section in ACTIVE_CONNECTORS):
         logging.info("No configuration found for %s", BEAUTIFUL_CONNECTORS)
         sys.exit(1)
 
     connectors = scraparr.connectors.Connectors()
 
-    for section in config.sections():
+    for section in CONFIG.sections():
         if section in ACTIVE_CONNECTORS:
-            connectors.add_connector(section.lower(), dict(config.items(section)))
+            connectors.add_connector(section.lower(), dict(CONFIG.items(section)))
 
     try:
         def run_server():
             """Starts the WSGI server"""
-            httpd = make_server(address, port, app)
+            httpd = make_server(ADDRESS, PORT, app)
             httpd.serve_forever()
 
         server_thread = threading.Thread(target=run_server)
