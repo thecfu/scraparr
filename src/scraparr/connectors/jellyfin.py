ec6b0252d1b8799ddd6d2d9604d00f56ed5f99e6
TheGameProfi <mbg14.gaming@gmail.com>
2025-04-21 15:17:59 +0200

    Refactor Codebase and Improve Prometheus Integration - Fix metrics handling: use Prometheus `Gauge` correctly - Add Jellyfin configuration support in `config.yml` - Add and update function/class docstrings for better clarity - Remove unnecessary code comments and debug print/logging statements - Add return check to return `None` on scrape failure - Refactor code to comply with `pylint` standards

--- src/scraparr/connectors/jellyfin.py
+++ src/scraparr/connectors/jellyfin.py
@@ -1,87 +1,85 @@
 """
-Module to handle the Metrics of the Whisparr Service
+Module to handle the Metrics of the Jellyfin Service
 """
 import time
-
-import requests 
-import scraparr.metrics.jellyfin as jellyfin_metrics
 import logging
+import requests
+import scraparr.metrics.jellyfin as jellyfin_metrics
 from scraparr.metrics.general import UP
 
+QUERY = "SortBy=SortName%2CProductionYear&SortOrder=Ascending&Recursive=true"
 
 def get_header(api_key):
+    """Translate the API Key into a Header for Jellyfin"""
     token = f"Mediabrowser Token={api_key}"
     return {"Authorization": token}
 
-
 def get_number_of_devices(url, headers_auth, alias):
-    """Grab the Indexers from the Jellyfin Endpoint"""
-    try:        
-        res = requests.get(f"{url}/Devices", headers=headers_auth, timeout=10)  # Adding timeout
+    """Grab the Devices from the Jellyfin Endpoint"""
+    try:
+        res = requests.get(f"{url}/Devices", headers=headers_auth, timeout=10)
         res.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
         return res.json()['TotalRecordCount']
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-   
+        return None
 
 
-
-def getGenres(url, headers_auth, alias):
+def get_genres(url, headers_auth, alias):
+    """Grab the Genres from the Jellyfin Endpoint"""
     try:
-        
-        res = requests.get(f"{url}/Genres", headers=headers_auth, timeout=10)  # Adding timeout
+        res = requests.get(f"{url}/Genres", headers=headers_auth, timeout=10)
         res.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
         data = res.json()
         genre_names = [item["Name"] for item in data.get("Items", [])]
                 
         return genre_names
     except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")    
-        
-        
+        print(f"Request failed: {e}")
+        return None
+
 def get_number_of_user(url, headers_auth, alias):
-    """Grab the Indexers from the Jellyfin Endpoint"""
+    """Grab the Users from the Jellyfin Endpoint"""
     try:
-        
-        res = requests.get(f"{url}/Users", headers=headers_auth, timeout=10)  # Adding timeout
+        res = requests.get(f"{url}/Users", headers=headers_auth, timeout=10)
         res.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
-        #print(res.json())  # or res.text if it's not JSON
         data = res.json()
-       
         return len(data)
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-   
+        return None
+
 def get_number_of_movies(url, headers_auth, alias):
-    """Grab the Indexers from the Jellyfin Endpoint"""
+    """Grab the Movies from the Jellyfin Endpoint"""
     try:
-        
-        res = requests.get(f"{url}/Items?SortBy=SortName%2CProductionYear&SortOrder=Ascending&IncludeItemTypes=Movie&Recursive=true", headers=headers_auth, timeout=10)  # Adding timeout
+        res = requests.get(f"{url}/Items?{QUERY}&IncludeItemTypes=Movie",
+                           headers=headers_auth, timeout=10)
         res.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
        
         return res.json()['TotalRecordCount']
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-   
+        return None
+
 def get_number_of_series(url, headers_auth, alias):
-    """Grab the Indexers from the Jellyfin Endpoint"""
+    """Grab the Series from the Jellyfin Endpoint"""
     try:
-        
-        res = requests.get(f"{url}/Items?SortBy=SortName%2CProductionYear&SortOrder=Ascending&IncludeItemTypes=Series&Recursive=true", headers=headers_auth, timeout=10)  # Adding timeout
+        res = requests.get(f"{url}/Items?{QUERY}&IncludeItemTypes=Series",
+                           headers=headers_auth, timeout=10)
         res.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
        
         return res.json()['TotalRecordCount']
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
-   
-   
+        return None
+
 def scrape(config):
+    """Scrape the Jellyfin Service"""
     initial_time = time.time()
-    """Scrape the Bazarr Service"""
     url = config.get('url')
     api_key = config.get('api_key')
-    alias = config.get('alias', 'bazarr')
-    
+    alias = config.get('alias', 'jellyfin')
+
     headers_auth=get_header(api_key)
     
     n_devices = get_number_of_devices(url, headers_auth,alias)
@@ -92,17 +90,23 @@ def scrape(config):
     end_time = time.time()
     jellyfin_metrics.LAST_SCRAPE.labels(alias).set(end_time)
     jellyfin_metrics.SCRAPE_DURATION.labels(alias).set(end_time - initial_time)
-    
 
-    logging.info("Passing through Jellyfin Scrape")
-    return{"n_devices": n_devices, "n_user": n_user, "n_movies": n_movies, "n_series": n_series}
+    if any((n_devices, n_user, n_movies, n_series, genres) is None):
+        logging.error("No Data found for Jellyfin, assuming Failure")
+        return None
+    return{
+        "n_devices": n_devices,
+        "n_user": n_user,
+        "n_movies": n_movies,
+        "n_series": n_series,
+        "genres": genres
+    }
 
 def update_metrics(data, detailed, alias):
-    """Update the Metrics for the Jellyseerr Service"""
-    jellyfin_metrics.JELLYFIN_NUMBER_OF_DEVICES = data["n_devices"]
-    jellyfin_metrics.JELLYFIN_NUMBER_OF_USERS = data["n_devices"]
-    jellyfin_metrics.JELLYFIN_NUMBER_OF_MOVIES = data["n_movies"]
-    jellyfin_metrics.JELLYFIN_NUMBER_OF_SERIES = data["n_series"]
+    """Update the Metrics for the Jellyfin Service"""
+    jellyfin_metrics.JELLYFIN_NUMBER_OF_DEVICES.labels(alias).set(data["n_devices"])
+    jellyfin_metrics.JELLYFIN_NUMBER_OF_USERS.labels(alias).set(data["n_devices"])
+    jellyfin_metrics.JELLYFIN_NUMBER_OF_MOVIES.labels(alias).set(data["n_movies"])
+    jellyfin_metrics.JELLYFIN_NUMBER_OF_SERIES.labels(alias).set(data["n_series"])
 
-    logging.info("Updating Jellyfin metrics")
 
--- src/scraparr/metrics/jellyfin.py
+++ src/scraparr/metrics/jellyfin.py
@@ -1,4 +1,4 @@
-from prometheus_client import Gauge, Enum
+from prometheus_client import Gauge
 
 # Scraping Stats
 LAST_SCRAPE = Gauge('jellyfin_last_scrape', 'Last time Jellyfin was scraped', ['alias'] )
@@ -7,7 +7,7 @@ SCRAPE_DURATION = Gauge('jellyfin_scrape_duration', 'Duration of Jellyfin scrape
 # Status Stats
 START_TIME = Gauge('jellyfin_start_time', 'Jellyfin start time', ['alias'] )
 BUILD_TIME = Gauge('jellyfin_build_time', 'Jellyfin build time', ['alias'] )
-JELLYFIN_NUMBER_OF_DEVICES = ('jellyfin_number_of_devices', 'Jellyfin number of devices', ['alias'])
-JELLYFIN_NUMBER_OF_USERS = ('jellyfin_number_of_users', 'Jellyfin number of users', ['alias'])
-JELLYFIN_NUMBER_OF_MOVIES = ('jellyfin_number_of_movies', 'Jellyfin number of movies', ['alias'])
-JELLYFIN_NUMBER_OF_SERIES = ('jellyfin_number_of_series', 'Jellyfin number of series', ['alias'])
+JELLYFIN_NUMBER_OF_DEVICES = Gauge('jellyfin_number_of_devices', 'Jellyfin number of devices', ['alias'])
+JELLYFIN_NUMBER_OF_USERS = Gauge('jellyfin_number_of_users', 'Jellyfin number of users', ['alias'])
+JELLYFIN_NUMBER_OF_MOVIES = Gauge('jellyfin_number_of_movies', 'Jellyfin number of movies', ['alias'])
+JELLYFIN_NUMBER_OF_SERIES = Gauge('jellyfin_number_of_series', 'Jellyfin number of series', ['alias'])
