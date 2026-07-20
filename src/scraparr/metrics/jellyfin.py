860477d10219323891dab98729a705a168aefbe4
Orvexᴮᴼᵀ <automation@thecfu.de>
2026-07-20 07:08:31 +0000

    feat: add session detail and client info metric definitions

--- src/scraparr/metrics/jellyfin.py
+++ src/scraparr/metrics/jellyfin.py
@@ -16,3 +16,32 @@ GENRES = Gauge('jellyfin_genres_total', 'Jellyfin number of genres', ['alias', '
 
 SESSIONS_T = Gauge('jellyfin_sessions_total', 'Jellyfin number of sessions within Configured amount of Seconds', ['alias'])
 SESSIONS = Gauge('jellyfin_sessions', 'Jellyfin number of sessions within Configured amount of Seconds', ['alias', 'user_id', 'user_name'])
+
+# Session Details
+SESSION_LABELS = ['alias', 'username', 'device', 'client', 'type', 'method']
+
+SESSION_POSITION = Gauge(
+    'jellyfin_session_position_seconds',
+    'Current playback position in seconds',
+    SESSION_LABELS)
+SESSION_DURATION = Gauge(
+    'jellyfin_session_duration_seconds',
+    'Total media duration in seconds',
+    SESSION_LABELS)
+SESSION_TRANSCODING = Gauge(
+    'jellyfin_session_transcoding',
+    '1 if session is transcoding, 0 otherwise',
+    SESSION_LABELS)
+SESSION_PAUSED = Gauge(
+    'jellyfin_session_paused',
+    '1 if session is paused, 0 if playing',
+    SESSION_LABELS)
+SESSION_BITRATE = Gauge(
+    'jellyfin_session_bitrate_bits',
+    'Current stream bitrate in bits per second',
+    SESSION_LABELS)
+
+SESSION_CLIENT_INFO = Gauge(
+    'jellyfin_session_client_info',
+    'Info gauge for active session client versions',
+    ['alias', 'username', 'device', 'client', 'client_version'])
